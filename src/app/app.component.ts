000993757c491c221df53afb88331ceed14ba5be
gungrahi <enthusiast.sahil@gmail.com>
2024-03-23 22:12:02 +0530

    used role=status and rxjs operator combineLatest for screen reader announcement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DataProviderService } from './services/data-provider.service';
 import { CommonModule } from '@angular/common';
-import { combineLatest, take } from 'rxjs';
+import { combineLatest, map, take } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -13,32 +13,36 @@ import { combineLatest, take } from 'rxjs';
 })
 export class AppComponent implements OnInit {
   title = "";
-  headerText: string | undefined;
-  loaderText: string | undefined;
-  @ViewChild("srAnnouncement") el!: ElementRef;
+  @ViewChild("srLiveRegionContainer") srLiveRegionContainer!: ElementRef<HTMLElement>;
 
   constructor(private dataService: DataProviderService) {}
   ngOnInit(): void {
     const ob1$ = this.dataService.getHeading();
     const ob2$ = this.dataService.getA11yText();
     ob1$.subscribe((val) => {
-      // this.headerText = val ?? "";
       this.title = val;
+      // some logic
     });
     ob2$.subscribe((val) => {
-      // this.loaderText = val ?? "page has been loaded";
+      // some logic
     });
-    combineLatest({
+    const srAnnouncement$ = combineLatest({
       a: ob1$,
       b: ob2$
     }).pipe(take(1))
-      .subscribe(res => {
-        this.headerText = res.a ?? "";
-        this.loaderText = res.b ?? "page has been loaded";
-        setTimeout(() => {
-          (this.el.nativeElement as HTMLElement).setAttribute("aria-hidden", "true");
-        }, 10);
-      });
+      .pipe(map((res => {
+        const headerText = res.a ?? "";
+        const loaderText = res.b ?? "page has been loaded";
+        return headerText + " " + loaderText;
+      })));
+    srAnnouncement$.subscribe((announcement) => {
+      let el = document.createElement("p");
+      el.innerHTML = announcement;
+      this.srLiveRegionContainer.nativeElement.appendChild(el);
+      setTimeout(() => {
+        this.srLiveRegionContainer.nativeElement.setAttribute("aria-hidden", "true");
+      }, 500);
+    });
   }
 
 }
